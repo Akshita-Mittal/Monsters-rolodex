fa2b50ea3d39c9d0d88f241410b246ca3a6f297f
Akshita mittal <you@example.com>
2020-09-10 13:20:57 +0530

    Updates

---
+++ precache-manifest.9af901cf9a4d65967fb6b0db165e3369.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4dfd823da4e13ad299be64131f175738",
+    "url": "/Monsters-rolodex/index.html"
+  },
+  {
+    "revision": "4442e842a3f5f9848874",
+    "url": "/Monsters-rolodex/static/css/main.46281eec.chunk.css"
+  },
+  {
+    "revision": "f7b84cd810271619eca9",
+    "url": "/Monsters-rolodex/static/js/2.7d94dc6b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Monsters-rolodex/static/js/2.7d94dc6b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4442e842a3f5f9848874",
+    "url": "/Monsters-rolodex/static/js/main.6f25e4f7.chunk.js"
+  },
+  {
+    "revision": "37d1d950e0fa32c52be3",
+    "url": "/Monsters-rolodex/static/js/runtime-main.eecc3a94.js"
+  }
+]);
